c703e9bc4f068e0cae2a1ef4d148c442cf0d5ddd
Afia1102 <fmshtq@gmail.com>
2024-08-13 21:14:02 +0500

    saving file

--- basic_calculator_operations_in_python.py
+++ basic_calculator_operations_in_python.py
@@ -1,14 +1,29 @@
 #Basic Calculator Operations in Python
 
+def add(input_1,input_2):
+    return input_1+input_2
+
+def sub(input_1,input_2):
+    return input_1+input_2
+
+def multi(input_1,input_2):
+    return input_1+input_2
+
+def div(input_1,input_2):
+    return input_1+input_2
+
 input_1=int(input("Enter first number="))
 input_2=int(input("Enter second number="))
-operation=input("Enter the operation(+,-,*,/)")
+operation=input("Enter the operation(+,-,*,/)=")
+
 
 if operation=='+':
-    return input_1 + input_2
+    print("Output={}".format(add(input_1,input_2)))
 elif operation=='-':
-    return input_1 - input_2
+    print("Output={}".format(sub(input_1,input_2)))
 elif operation=='*':
-    return input_1 - input_2
+    print("Output={}".format(multi(input_1,input_2)))
 elif operation=='/':
-    return input_1 - input_2
+    print("Output={}".format(div(input_1,input_2)))
+else:
+    print("Invalid input")
