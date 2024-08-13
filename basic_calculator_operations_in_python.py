8f0ba1e43ae7b94a5ce67c8433c22349c2e5233b
Afia1102 <fmshtq@gmail.com>
2024-08-13 21:00:11 +0500

    adding file

---
+++ basic_calculator_operations_in_python.py
@@ -0,0 +1,14 @@
+#Basic Calculator Operations in Python
+
+input_1=int(input("Enter first number="))
+input_2=int(input("Enter second number="))
+operation=input("Enter the operation(+,-,*,/)")
+
+if operation=='+':
+    return input_1 + input_2
+elif operation=='-':
+    return input_1 - input_2
+elif operation=='*':
+    return input_1 - input_2
+elif operation=='/':
+    return input_1 - input_2
