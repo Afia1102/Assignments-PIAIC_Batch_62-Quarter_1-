412c30151003e5a86dd7bd57311bdff60ea3df50
Afia1102 <fmshtq@gmail.com>
2024-08-13 21:59:39 +0500

    final commit

--- basic_calculator_operations_in_python.py
+++ basic_calculator_operations_in_python.py
@@ -1,29 +1,47 @@
-#Basic Calculator Operations in Python
 
-def add(input_1,input_2):
-    return input_1+input_2
-
-def sub(input_1,input_2):
-    return input_1+input_2
-
-def multi(input_1,input_2):
-    return input_1+input_2
-
-def div(input_1,input_2):
-    return input_1+input_2
-
-input_1=int(input("Enter first number="))
-input_2=int(input("Enter second number="))
-operation=input("Enter the operation(+,-,*,/)=")
+#BasicCalculatorOperationsInPython
 
 
-if operation=='+':
-    print("Output={}".format(add(input_1,input_2)))
-elif operation=='-':
-    print("Output={}".format(sub(input_1,input_2)))
-elif operation=='*':
-    print("Output={}".format(multi(input_1,input_2)))
-elif operation=='/':
-    print("Output={}".format(div(input_1,input_2)))
-else:
-    print("Invalid input")
+def main():
+    while True:
+        try:
+            first_input = input("Enter first number (or press Enter to exit): ")
+            if first_input == '':
+                print("Exit.")
+                break
+            input_1 = int(first_input)  
+            input_2 = int(input("Enter second number: "))
+            operation = input("Enter the operation (+, -, *, /): ")
+            if operation == '+':
+                print("Output: {}".format(add(input_1, input_2)))
+            elif operation == '-':
+                print("Output: {}".format(sub(input_1, input_2)))
+            elif operation == '*':
+                print("Output: {}".format(multi(input_1, input_2)))
+            elif operation == '/':
+                print("Output: {}".format(div(input_1, input_2)))
+            else:
+                print("Error: Invalid operation")
+        
+        except ValueError:
+            print("Error: Invalid input. Please enter numeric values.")
+
+
+
+
+def add(input_1, input_2):
+    return input_1 + input_2
+
+def sub(input_1, input_2):
+    return input_1 - input_2
+
+def multi(input_1, input_2):
+    return input_1 * input_2
+
+def div(input_1, input_2):
+    if input_2 != 0:
+        return input_1 / input_2
+    else:
+        return "Error: Division by zero"
+
+main()
